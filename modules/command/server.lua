e0e930946bd3b7fbd13d82704f7430e73e8c1601
ngschaider <niklas.gschaider@gschaider-systems.at>
2022-09-02 09:41:06 +0000

    rewrote command module

--- modules/command/server.lua
+++ modules/command/server.lua
@@ -1,91 +1,45 @@
-local User = M("user");
-local Character = M("character");
-local logger = M("logger");
-local job = M("job");
-local event = M("event");
-
-local registeredCommands = {};
-local validators = {};
-local valueConverters = {};
-
-local convert = function(value, type, args)
-	if valueConverters[type] then
-		return validators[type](value, table.unpack(args));
-	else
-		return value;
-	end
-end;
-
-local validate = function(value, type, args)
-	if validators[type] then
-		return validators[type](value, table.unpack(args));
-	else
-		return true;
-	end
-end;
-
--- we dont have a chat for now
---[[
-local addSuggestion = function(commandName, info)
-	local suggestionArgs = {};
-
-	for k,v in pairs(info.arguments) do
-		table.insert(suggestionArgs, {
-			name = info.name or "",
-			help = info.help or "",
-		})
+local commands = {};
+module.registerCommand = function(name, cb, options)
+	if not options then
+		options = {};
 	end
 
-	TriggerClientEvent("chat:addSuggestion", -1, "/" .. commandName, info.help, suggestionArgs);
-end;
-]]
+	local command = {
+		name = name,
+		cb = cb,
+		options = options,
+	};
 
-module.registerCommand = function(commandName, cb, restricted, info)
-	info = info or {};
-	info.arguments = info.arguments or {};
-	info.help = info.help or {};
-	info.rawArgs = info.rawArgs or false;
+	RegisterCommand(command.name, function(...)
+		onCommandExecuted(command, ...);
+	end, false);
+end
 
-	RegisterCommand(commandName, function(playerId, args, rawCommand)
-		local user = User:GetByPlayerId(playerId);
-		if info.rawArgs then
-			cb(user, args);
-		else
-			local newArgs = {};
+local convertArgs = function(argument, config)
+	if config.type == "user" then
+		return User.GetById(argument);
+	elseif config.type == "character" then
+		return Character.GetById(argument);
+	elseif config.type == "item" then
+		return Item.GetbyId(argument);
+	end
+end
 
-			for k,v in pairs(info.arguments) do
-				newArgs[v.name] = args[k];
+local onCommandExecuted = function(command, playerId, args, raw)
+	local user = User.GetByPlayerId(playerId);
 
-				if v.type then
-					local valid = validate(newArgs[v.name], v.type, v.args);
-					if not valid then
-						logger.info("Invalid command argument " .. newArgs[v.name]);
-						return;
-					end
+	if options.raw then
+		cb(user, raw);
+		return;
+	end
 
-					newArgs[v.name] = convert(newArgs[v.name], v.type, v.args);
-				end
-			end
-			
-			cb(user, newArgs);
+	if options.args then
+		local convertedArgs = {};
+		for i=1,#options.args,1 do
+			local convertedArg = convertArg(args[i], options.args[i]);
+			table.insert(convertedArgs, convertedArg);
 		end
-	end, restricted);
-
-	logger.info("Registered command '" .. commandName .. "'");
-
-	--addSuggestion(commandName, info);
-	table.insert(registeredCommands, {
-		name = commandName,
-		info = info,
-	});
-end;
-
-
--- we dont have a chat for now -> we dont need any suggestions for now
---[[
-event.onClient("base:playerJoined", function()
-	for k,v in pairs(registeredCommands) do
-		addSuggestion(v.name, v.info);
+		cb(user, args);
+		return;
 	end
-end);
-]]
+end

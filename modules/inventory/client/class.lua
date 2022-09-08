d34f59c3d892f51632b5959bd36a3c19f0234bdf
ngschaider <niklas.gschaider@gschaider-systems.at>
2022-09-08 11:29:40 +0000

    integrated inventory_ui into inventory module

---
+++ modules/inventory/client/class.lua
@@ -0,0 +1,39 @@
+local callback = M("core").callback;
+local Item = M("item");
+local class = M("class");
+local logger = M("core").logger;
+local utils = M("utils");
+local core = M("core");
+
+local Inventory = class("Inventory", core.SyncObject);
+core.RegisterSyncClass(Inventory);
+
+function Inventory:initialize(id)
+	core.SyncObject.initialize(self, "Inventory", id, "inventories");
+end
+
+function Inventory:getMaxWeight()
+	return self:getData("maxWeight");
+end
+
+function Inventory:getItemIds()
+	return self:rpc("getItemIds");
+end
+
+function Inventory:getItems()
+	logger.debug("inventory", "getItems", "getting item ids");
+	local ids = self:getItemIds();
+	logger.debug("inventory", "getItems", "ids", json.encode(ids));
+	
+	local items = utils.table.mapValues(ids, function(id)
+		logger.debug("inventory", "getItems", "id", id);
+		return Item.GetById(id);
+	end);
+
+	return items;
+end
+
+module.GetById = function(id)
+	logger.debug("inventory", "module.GetById", "id", id);
+	return core.GetSyncObject("Inventory", id);
+end;
---
+++ modules/inventory/server/class.lua
@@ -0,0 +1,57 @@
+local utils = M("utils");
+local callback = M("core").callback;
+local logger = M("core").logger;
+local class = M("class");
+local Item = M("item");
+local core = M("core");
+
+local Inventory = class("Inventory", core.SyncObject);
+core.RegisterSyncClass(Inventory);
+
+function Inventory:initialize(id)
+	core.SyncObject.initialize(self, "Inventory", id, "inventories");
+
+	self:syncProperty("maxWeight", true, false);
+	self:rpcMethod("getItemIds", true);
+end
+
+function Inventory:getMaxWeight()
+	return self:getData("maxWeight");
+end;
+
+function Inventory:setMaxWeight(maxWeight)
+	self:setData("maxWeight", maxWeight);
+end;
+
+function Inventory:getItemIds()
+	local results = MySQL.query.await("SELECT id FROM items WHERE inventoryId=?", {self.id});
+	
+	local ids = utils.table.mapValues(results, function(v)
+		return v.id;
+	end);
+	logger.debug("inventory", "getItemIds", "ids", json.encode(ids));
+	return ids;
+end;
+
+function Inventory:getItems()
+	local ids = self:getItemIds();
+
+	local items = utils.table.mapValues(ids, function(id)
+		return Item.GetById(id);
+	end);
+	
+	return items;
+end;
+
+module.Create = function(maxWeight)
+	local id = MySQL.insert.await("INSERT INTO inventories (max_weight) VALUES (?)", {
+		maxWeight,
+	});
+
+	return module.GetById(id);
+end
+
+module.GetById = function(id)
+	logger.debug("inventory", "module.GetById", "id", id);
+	return core.GetSyncObject("Inventory", id);
+end;
